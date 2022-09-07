f1b1c8404f74a9c7f475b44394b8f88ef00fb899
Bartek <bartek62345@gmail.com>
2022-09-07 13:13:33 +0200

    Comments deleting system

--- src/components/Posts/Comments.js
+++ src/components/Posts/Comments.js
@@ -1,9 +1,25 @@
 import Card from "../UI/Card";
 import CreateComment from "./CreateComment";
 import style from "./Comments.module.css";
+import { useSelector } from "react-redux";
+import usePost from "../../hooks/usePost";
 
 function Comments(props) {
   const { comments } = props;
+  const user = useSelector((state) => state.user);
+  const { setComment } = usePost();
+
+  const deleteCommentHandler = function (e) {
+    const commentIndex = +e.target.dataset.index;
+
+    const newComments = [
+      ...comments.slice(0, commentIndex),
+      ...comments.slice(commentIndex + 1),
+    ];
+
+    props.updateComments(newComments);
+    setComment(props.postId, newComments);
+  };
 
   let commentsItems = (
     <Card>
@@ -15,7 +31,18 @@ function Comments(props) {
     commentsItems = comments.map((comment, i) => (
       <li key={"c" + i} className={style.comment}>
         <Card>
-          <h5>{comment.author}</h5>
+          <header className={style["comment-header"]}>
+            <h5>{comment.author}</h5>
+            {user.username === comment.author && (
+              <button
+                className="material-symbols-outlined"
+                onClick={deleteCommentHandler}
+                data-index={i}
+              >
+                delete
+              </button>
+            )}
+          </header>
           <p>{comment.content}</p>
         </Card>
       </li>
