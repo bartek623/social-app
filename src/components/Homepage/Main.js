cca89e2fef9093712d68403ff76759650cbbbbde
Bartek <bartek62345@gmail.com>
2022-09-09 18:35:44 +0200

    Preparing for deployment process

--- src/components/Homepage/Main.js
+++ src/components/Homepage/Main.js
@@ -1,29 +1,39 @@
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import NavPanel from "../Navigation/NavPanel";
 import Content from "./Content";
-import Profile from "../Profile/Profile";
-import LikedPosts from "../LikedPosts/LikedPosts";
-import Explore from "../Explore/Explore";
+// import Profile from "../Profile/Profile";
+// import LikedPosts from "../LikedPosts/LikedPosts";
+// import Explore from "../Explore/Explore";
+// import FriendsList from "../FriendsList/FriendsList";
 import PageNotFound from "../PageNotFound/PageNotFound";
+import LoadingBars from "../UI/LoadingBars";
 
 import style from "./Main.module.css";
-import FriendsList from "../FriendsList/FriendsList";
+
+// Lazy loading
+const Profile = React.lazy(() => import("../Profile/Profile"));
+const LikedPosts = React.lazy(() => import("../LikedPosts/LikedPosts"));
+const Explore = React.lazy(() => import("../Explore/Explore"));
+const FriendsList = React.lazy(() => import("../FriendsList/FriendsList"));
 
 function Main() {
   return (
     <main className={style.main}>
       <NavPanel />
-      <Routes>
-        <Route path="/home" element={<Content />} />
-        <Route path="/profile/:userId" element={<Profile />} />
-        <Route path="/friends-list" element={<FriendsList />} />
-        {/* Two different Routes for the same element because react router v6 no longer supports optional params */}
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/explore/:tag" element={<Explore />} />
-        <Route path="/liked" element={<LikedPosts />} />
-        <Route path="/*" element={<PageNotFound />} />
-      </Routes>
+      <Suspense fallback={<LoadingBars />}>
+        <Routes>
+          <Route path="/home" element={<Content />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+          <Route path="/friends-list" element={<FriendsList />} />
+          {/* Two different Routes for the same element because react router v6 no longer supports optional params */}
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/explore/:tag" element={<Explore />} />
+          <Route path="/liked" element={<LikedPosts />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
     </main>
   );
 }
