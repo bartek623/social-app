b24c657ce24c432c9e93b27f3f529dcbcc2ef1f4
Bartek <bartek62345@gmail.com>
2022-09-09 19:23:03 +0200

    testing after reverting deployment changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
 import { Provider } from "react-redux";
 import store from "./store/store";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
