089df6fedb5189dbf3feb405dd0fe89fbe721caf
Bartek <bartek62345@gmail.com>
2022-09-09 18:48:06 +0200

    Fixed routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
 import store from "./store/store";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
