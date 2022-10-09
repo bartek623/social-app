1be72bb39fad4f019d5c40410cfe7c55fc85db4a
Bartek <bartek62345@gmail.com>
2022-10-09 16:51:25 +0200

    Posts number optimalization

--- src/components/Posts/PostsList.js
+++ src/components/Posts/PostsList.js
@@ -4,23 +4,45 @@ import Post from "./Post";
 import themeStyle from "../UI/theme.module.css";
 import style from "./PostsList.module.css";
 
+const POSTS_INCREASE = 10;
+
 function PostsList(props) {
+  // How many posts are visible? (default 10)
+  const [showPosts, setShowPosts] = useState(POSTS_INCREASE);
   const [posts, setPosts] = useState(props.posts);
   const { deletePost } = usePost();
+
+  const currentPosts = posts.slice(0, showPosts);
 
   const deleteHandler = function (postId) {
     deletePost(postId);
     setPosts((prevState) => prevState.filter((post) => post.postId !== postId));
   };
 
-  const postsTransformed = posts.map((post, i) => (
+  const postsTransformed = currentPosts.map((post, i) => (
     <Post key={"p" + i} post={post} onPostDelete={deleteHandler} />
   ));
 
+  const showMorePosts = function () {
+    setShowPosts((prev) => {
+      const newPostsNumber = prev + POSTS_INCREASE;
+      if (newPostsNumber < posts.length) return newPostsNumber;
+
+      if (newPostsNumber >= posts.length) return posts.length;
+    });
+  };
+
   return (
-    <ul className={`${style["posts-list"]} ${themeStyle.container}`}>
-      {postsTransformed}
-    </ul>
+    <>
+      <ul className={`${style["posts-list"]} ${themeStyle.container}`}>
+        {postsTransformed}
+      </ul>
+      {showPosts < posts.length && (
+        <button className={style["show-more-btn"]} onClick={showMorePosts}>
+          Show more
+        </button>
+      )}
+    </>
   );
 }
 
